d3f417b37445238dc43a33d5dc9129114b437190
jp-quintana <juanquintana1996@gmail.com>
2024-02-26 19:21:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# CODER SQL
+
+## Introducción
+
+Para el proyecto final se realizó una base de datos de una ecommerce simple, sobre el cual se buscaba llevar un registro de usuarios con sus ordenes y preferencias, y productos con sus categorias.
+
+### Objetivo
+
+El proyecto busca establecer un registro actualizado de los usuarios del ecommerce y como interactúan con los productos que ofrece. Esto abarca las acciones de agregar al carrito, crear ordenes, guardar productos como favoritos y escribir reseñas de estos. Por otro lado, se busca poder normalizar los datos de producto mediante una tabla de categoría (se buscará normalizar aun más en el futuro). Y por último, se busca tener todos los datos de la dirección de usuario a la hora de crear una orden, mediante la creación de una tabla "address" y otra puente llamada "user_address".
+
+### Situación problemática
+
+El uso de una base de datos en un ecommerce es fundamental porque permite almacenar y gestionar de manera eficiente una gran cantidad de información crítica, como inventario de productos (a agregar en un futuro), detalles de clientes, historial de pedidos y preferencias de compra. Esto facilita la personalización de la experiencia del usuario, agiliza los procesos de compra, permite análisis de datos para tomar decisiones estratégicas, garantiza la integridad y seguridad de los datos, y facilita la escalabilidad del negocio al adaptarse al crecimiento y la evolución de la empresa y sus necesidades.
+
+### Modelo de negocio
+
+La organización del modelo planteado para la base de datos del ecommerce busca recopilar, almacenar y administrar información sobre sus usuarios, almacenando sus datos personales, sus preferencias en cuanto a productos, sus reseñas de estos, etc.
+La idea de la base de datos es poder utilizar la información recopilada para generar decisiones sobre que productos lanzar en el futuro, determinar estrategias de descuentos/publicidad, quitar productos si no tienen el suficiente desempeño, entre otras cosas.
+También se busca que la dirección de los usuarios sea de facil acceso para agilizar el proceso de finalizar compra.
+
+## Diagrama Entidad Relación
+
+### Modelo EER
+
+### ERD
+
+## Diagrama Entidad Relación
---
+++ ecommerce-create.sql
@@ -0,0 +1,113 @@
+drop schema if exists ecommerce;
+create schema if not exists ecommerce;
+use ecommerce;
+
+create table user(
+	user_id int auto_increment primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    email varchar(30) not null,
+    birthdate date not null,
+    password varchar(30) not null,
+    created_at timestamp default current_timestamp
+);
+
+create table address(
+	address_id int auto_increment primary key,
+    city varchar(30) not null,
+    country varchar(30) not null,
+    zipcode varchar(10) not null,
+    state varchar(30) not null,
+    street_name varchar(30) not null,
+    street_number varchar(30) not null,
+    created_at timestamp default current_timestamp
+);
+
+create table user_address(
+	user_address_id int auto_increment primary key,
+    user_id int not null,
+    shipping_address_id int not null,
+    billing_address_id int not null,
+    created_at timestamp default current_timestamp,
+    foreign key (user_id) references user(user_id),
+    foreign key (shipping_address_id) references address(address_id),
+    foreign key (billing_address_id) references address(address_id)
+);
+
+create table category(
+	category_id int auto_increment primary key,
+	category_name varchar(30) not null,
+	created_at timestamp default current_timestamp
+);
+
+create table product(
+	product_id int auto_increment primary key,
+	product_name varchar(50) not null,
+	product_price int not null,
+	product_description varchar(300) not null,
+	category_id int not null,
+	created_at timestamp default current_timestamp,
+	foreign key (category_id) references category(category_id)
+);
+
+create table cart(
+	cart_id int auto_increment primary key,
+    user_id int not null,
+	created_at timestamp default current_timestamp,
+	foreign key (user_id) references user(user_id)
+);
+
+create table cart_item(
+	cart_item_id int auto_increment primary key,
+    quantity int not null check (quantity > 0),
+    product_id int not null,
+    cart_id int not null,
+    created_at timestamp default current_timestamp,
+    foreign key (product_id) references product(product_id),
+    foreign key (cart_id) references cart(cart_id)
+);
+
+create table `order`(
+	order_id int auto_increment primary key,
+    city varchar(30) not null,
+    country varchar(30) not null,
+    zipcode varchar(10) not null,
+    state varchar(30) not null,
+    street_name varchar(30) not null,
+    street_number varchar(30) not null,
+    user_id int not null,
+    created_at timestamp default current_timestamp,
+    foreign key (user_id) references user(user_id)
+);
+
+create table order_item(
+	order_item_id int auto_increment primary key,
+    quantity int not null check (quantity > 0),
+    product_id int not null,
+    order_id int not null,
+    created_at timestamp default current_timestamp,
+    foreign key (product_id) references product(product_id),
+    foreign key (order_id) references `order`(order_id)
+);
+
+create table review(
+	review_id int auto_increment primary key,
+    review_rating decimal(3,1) not null check (review_rating  >= 1 AND review_rating  <= 5),
+    review_comment varchar(300) not null,
+    user_id int not null,
+    product_id int not null,
+    created_at timestamp default current_timestamp,
+    foreign key (user_id) references user(user_id),
+    foreign key (product_id) references product(product_id)
+);
+
+create table favorite(
+	favorite_id int auto_increment primary key,
+    product_id int not null,
+    user_id int not null,
+	created_at timestamp default current_timestamp,
+	foreign key (product_id) references product(product_id),
+    foreign key (user_id) references user(user_id)
+);
+
+
